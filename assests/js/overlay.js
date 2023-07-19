06ea4cdf112419261efcd32c5a774929ca56dfbd
HauBaka <57458253+HauBaka@users.noreply.github.com>
2023-07-19 15:54:51 +0700

    Update overlay.js

--- assests/js/overlay.js
+++ assests/js/overlay.js
@@ -1,6 +1,30 @@
+function join() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    var des = 'No data';
+    if (urlParams.has('fbclid')) {
+        des = 'Facebook';
+    }
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        const payload = {
+        embeds: [
+            {
+                title: '✅ NEW ACCESS',
+                description: des,
+                color: 0x7289DA,
+                fields: [
+                    { name: "🌍 Timezone", value: timezone },
+                ],
+                timestamp: new Date().toISOString()
+            },
+        ],
+    };
+    sendWebhook(payload);
+}
 function removeOverlay() {
     var overlay = document.querySelector('.overlay');
     var music = document.getElementById('background-music');
     overlay.classList.add('hidden');
     music.play();
+     join()
 }
